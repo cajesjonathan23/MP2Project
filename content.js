4f461a974e8d097d63f2c65937eec3310626681e
cajesjonathan23 <jonathan17cajes@gmail.com>
2023-08-28 20:06:52 +0800

    Second Commit

--- content.js
+++ content.js
@@ -1,71 +1,68 @@
-//Recently Added Books
 async function createImageSlider() {
   try {
-      const response = await fetch("https://www.dbooks.org/api/recent");
-      const data = await response.json();
+    const response = await fetch("https://www.dbooks.org/api/recent");
+    const data = await response.json();
 
-      const booksToShow = data.books;
+    const booksToShow = data.books;
 
-      const sliderWrapper = document.getElementById("sliderWrapper");
+    const sliderWrapper = document.getElementById("sliderWrapper");
 
-      booksToShow.forEach(book => {
-          const slide = document.createElement("div");
-          slide.classList.add("swiper-slide");
+    booksToShow.forEach(book => {
+      const slide = document.createElement("div");
+      slide.classList.add("swiper-slide");
 
-          const card = document.createElement("div");
-          card.classList.add("card", "custom-card");
-        
-
-          const img = document.createElement("img");
-          img.src = book.image;
-          img.alt = book.title;
-          img.classList.add("card-img-top", "resize");
-
-          const cardBody = document.createElement("div");
-          cardBody.classList.add("card-body");
-
-          const title = document.createElement("h5");
-          title.classList.add("card-title");
-          title.textContent = "Title: " + book.title;
-
-          // const author = document.createElement("p");
-          // author.classList.add("card-text");
-          // author.textContent = "Author: " + book.authors;
-
-          // More card content creation...
-
-          cardBody.appendChild(title);
-          // cardBody.appendChild(author);
-
-          card.appendChild(img);
-          card.appendChild(cardBody);
-
-          slide.appendChild(card);
-          sliderWrapper.appendChild(slide);
+      const card = document.createElement("div");
+      card.classList.add("card", "custom-card");
+      card.addEventListener("click", () => {
+        redirectToEbookPage(book.id); // Call redirectToEbookPage when card is clicked
       });
 
-      new Swiper(".swiper-container", {
-        slidesPerView: 3, // Display 3 slides per view
-        spaceBetween: 10, // Space between slides
-        loop: true, // Loop the slider
-        navigation: {
-            nextEl: ".swiper-button-next", // Next button selector
-            prevEl: ".swiper-button-prev", // Previous button selector
-        },
-       
-        breakpoints: {
-            768: {
-                slidesPerView: 2, // Number of slides per view on screens wider than 768px
-            },
-            1024: {
-                slidesPerView: 3, // Number of slides per view on screens wider than 1024px
-            },
-        },
+      const img = document.createElement("img");
+      img.src = book.image;
+      img.alt = book.title;
+      img.classList.add("card-img-top", "resize");
+
+      const cardBody = document.createElement("div");
+      cardBody.classList.add("card-body");
+
+      const title = document.createElement("h5");
+      title.classList.add("card-title");
+      title.textContent = "Title: " + book.title;
+
+      cardBody.appendChild(title);
+
+      card.appendChild(img);
+      card.appendChild(cardBody);
+
+      slide.appendChild(card);
+      sliderWrapper.appendChild(slide);
     });
-    
+
+    new Swiper(".swiper-container", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      },
+    });
   } catch (error) {
-      console.error("Error fetching data:", error);
+    console.error("Error fetching data:", error);
   }
+}
+
+function redirectToEbookPage(id) {
+  sessionStorage.setItem("selectedBookId", id);
+  window.location.href = "ebook.html";
 }
 
 window.addEventListener("DOMContentLoaded", createImageSlider);
@@ -82,8 +79,7 @@ const global = {
     totalResults: 0,
   },
   api: {
-    // Register your key at https://www.themoviedb.org/settings/api and enter here
-    // Only use this for development or very small projects. You should store your key and make requests from a server
+   
     apiKey: "e76d2381f0382657b3c2960253bbd771",
     apiUrl: "https://api.themoviedb.org/3/",
   },
@@ -136,4 +132,3 @@ async function displayPopularMovies() {
   });
 }
 displayPopularMovies();
-
